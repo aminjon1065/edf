51e65b4d566bed78c1c170c0c482dde48a68af83
aMinjon1065 <59558450+aminjon1065@users.noreply.github.com>
2023-08-22 21:26:25 +0500

    0.0.1

--- src/helpers/CONSTANTS.js
+++ src/helpers/CONSTANTS.js
@@ -1,5 +1,5 @@
-export const API_APP = "http://185.177.2.167:8000/api/api/v1";
-export const PUBLIC_APP_URL_DOCUMENTS = "http://localhost:8000/storage/documents/";
+export const API_APP = "http://185.177.2.167:8000/api/v1";
+export const PUBLIC_APP_URL_DOCUMENTS = "http://185.177.2.167:8000/storage/documents/";
 export const PUBLIC_URL_BACKEND = "http://localhost:8000";
 export const JWTToken = localStorage.getItem('token');
 export const VITE_PUSHER_APP_ID = "local";
