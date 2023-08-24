b5c83e3ac0a0a3ccdff6e296e8e7aa5dd606b497
aMinjon1065 <59558450+aminjon1065@users.noreply.github.com>
2023-08-24 23:41:52 +0500

    0.0.1

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ window.Echo = new Echo({
     wsPort: 6001,
     forceTLS: false,
     disableStats: true,
-    authEndpoint: 'https://api.coes.site/broadcasting/auth',
+    authEndpoint: 'http://api.coes.site/broadcasting/auth',
     auth: {
         headers: {
             // Authorization: `Bearer ${token}`,
