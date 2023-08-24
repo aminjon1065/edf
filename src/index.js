4f5fad4ece13ccf50a3c6dc8f5ea77a0a1804b3e
aMinjon1065 <59558450+aminjon1065@users.noreply.github.com>
2023-08-25 02:58:46 +0500

    0.0.1

--- src/index.js
+++ src/index.js
@@ -11,13 +11,9 @@ import i18n from "./localization/i18n";
 
 window.Pusher = require('pusher-js');
 window.Echo = new Echo({
-    broadcaster: 'pusher',
-    key: VITE_PUSHER_APP_KEY,
-    cluster: VITE_PUSHER_APP_CLUSTER,
-    wsHost: VITE_PUSHER_HOST,
-    wsPort: 6001,
-    // forceTLS: false,
-    disableStats: true,
+    key: '7df99e1bf3471243c810',
+    cluster: 'ap1',
+    forceTLS: true
 });
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
