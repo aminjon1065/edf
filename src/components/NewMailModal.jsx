b83eaa5aad60c0ad8f33512ea4e46995e87da4cb
aMinjon1065 <59558450+aminjon1065@users.noreply.github.com>
2023-09-02 07:21:04 +0500

    0.0.1

--- src/components/NewMailModal.jsx
+++ src/components/NewMailModal.jsx
@@ -442,7 +442,13 @@ export default function Index({open, setOpen}) {
                                         </div>
                                         <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
                                             <button
-                                                disabled={!userSelected || !title || !typeDocumentTj || !typeDocumentRu}
+                                                disabled={
+                                                    !userSelected ||
+                                                    !title ||
+                                                    !typeDocumentTj ||
+                                                    !typeDocumentRu ||
+                                                    (control === true && !dateDone)
+                                                }
                                                 type="button"
                                                 className="disabled:bg-gray-300 inline-flex w-full justify-center rounded-md bg-sky-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-sky-500 sm:ml-3 sm:w-auto"
                                                 onClick={sendMailFN}
