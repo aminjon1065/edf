959ccf548cbdc246bd5f69ec6d6b4682f94ac0d6
aMinjon1065 <59558450+aminjon1065@users.noreply.github.com>
2023-09-25 21:48:10 +0500

    0.0.1

--- src/helpers/CONSTANTS.js
+++ src/helpers/CONSTANTS.js
@@ -11,6 +11,6 @@
 // export const VITE_PUSHER_APP_CLUSTER = "ap1";
 
 
-export const API_APP = "http://10.230.64.157/api/api/v1";
-export const PUBLIC_APP_URL_DOCUMENTS = "http://10.230.64.157/api/storage/documents/";
-export const PUBLIC_URL_BACKEND = "http://10.230.64.157/api";
+export const API_APP = "http://anmndoh.tj/api/api/v1";
+export const PUBLIC_APP_URL_DOCUMENTS = "http://anmndoh.tj/api/storage/documents/";
+export const PUBLIC_URL_BACKEND = "http://anmndoh.tj/api";
