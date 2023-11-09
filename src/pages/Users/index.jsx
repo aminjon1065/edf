52bb34029231f9b1e3bb32aca2f7de10ff8b0cfb
aMinjon1065 <59558450+aminjon1065@users.noreply.github.com>
2023-11-09 11:08:15 +0500

    0.0.1

--- src/pages/Users/index.jsx
+++ src/pages/Users/index.jsx
@@ -62,7 +62,7 @@ const Index = () => {
                                 </td>
                                 <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
                                     <div className="text-gray-900">{t(person.region)}</div>
-                                    <div className="text-gray-500">{t(person.department)}</div>
+                                    <div className="text-gray-500">Test Department</div>
                                 </td>
                                 <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
                         <span
